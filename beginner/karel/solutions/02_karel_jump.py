bc475b773fa1d8a83987a80e63639af7a8323862
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-01-01 21:23:40 +0100

    beginner/karel/worlds/02_karel_jump: Fix description and add high score

--- beginner/karel/solutions/02_karel_jump.py
+++ beginner/karel/solutions/02_karel_jump.py
@@ -7,21 +7,22 @@ Instead, try to use if conditions and while loops in order to reduce the code yo
 
 You can also define extra functions for structuring your code.
 
-Current high score: ? lines of code
+Current high score: 15 lines of code
 """
 
 
 def main():
-    #####################################################################################
-    # This function contains the code which will control the karel robot.               #
-    # For this task you might need the following functions:                             #
-    #   move()              Move karel one field in the direction it is facing          #
-    #   turn_left()         Turn karel left (90 degrees)                                #
-    #   front_is_clear()    Returns True if Karel could move on the field it is facing  #
-    #   front_is_blocked()  Returns False if Karel could move on the field it is facing #
-    #   right_is_clear()    Returns True if Karel could move on the field to its right  #
-    #   right_is_blocked()  Returns False if Karel could move on the field to its right #
-    #   left_is_clear()     Returns True if Karel could move on the field to its left   #
-    #   left_is_blocked()   Returns False if Karel could move on the field to its left  #
-    #####################################################################################
+    ##############################################################################################################
+    # This function contains the code which will control the karel robot.                                        #
+    # For this task you might need the following functions:                                                      #
+    #   move()                   Move karel one field in the direction it is facing                              #
+    #   turn_left()              Turn karel left (90 degrees)                                                    #
+    #   front_is_clear()         Returns True if Karel could move on the field it is facing                      #
+    #   front_is_blocked()       Returns False if Karel could move on the field it is facing                     #
+    #   right_is_clear()         Returns True if Karel could move on the field to its right                      #
+    #   right_is_blocked()       Returns False if Karel could move on the field to its right                     #
+    #   left_is_clear()          Returns True if Karel could move on the field to its left                       #
+    #   left_is_blocked()        Returns False if Karel could move on the field to its left                      #
+    #   corner_color_is('Green') Returns True if the color of the field which Karel is standing on is GREEN      #
+    ##############################################################################################################
     pass
---
+++ beginner/karel/solutions/color_test.py
@@ -0,0 +1,8 @@
+from stanfordkarel import *
+
+
+def main():
+    if corner_color_is('Blue'):
+        print("color is blue")
+    else:
+        paint_corner(RED)
