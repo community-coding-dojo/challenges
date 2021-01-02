6f9626fa0995844f85774e9c6278c69aa76a1fda
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-01-02 13:16:42 +0100

    beginner/karel: Add task 06_another_labyrinth

---
+++ beginner/karel/solutions/06_another_labyrinth.py
@@ -0,0 +1,29 @@
+from stanfordkarel import *
+
+
+"""
+Oh no! Karel is lost - again! Help it to find a way through the labyrinth.
+
+Hint: Place beepers to remember, where you've been already ;)
+
+high score: ? Karel commands
+"""
+
+
+def main():
+    #########################################################################################################
+    # This function contains the code which will control the karel robot.                                   #
+    # For this task you might need the following functions:                                                 #
+    #   move()                   Move karel one field in the direction it is facing                         #
+    #   turn_left()              Turn karel left (90 degrees)                                               #
+    #   front_is_clear()         Returns True if Karel could move on the field it is facing                 #
+    #   front_is_blocked()       Returns False if Karel could move on the field it is facing                #
+    #   right_is_clear()         Returns True if Karel could move on the field to its right                 #
+    #   right_is_blocked()       Returns False if Karel could move on the field to its right                #
+    #   left_is_clear()          Returns True if Karel could move on the field to its left                  #
+    #   left_is_blocked()        Returns False if Karel could move on the field to its left                 #
+    #   corner_color_is('Green') Returns True if the color of the field which Karel is standing on is GREEN #
+    #   beepers_present()   Returns True if Karel is standing on a beeper                                   #
+    #   put_beeper()        Makes Karel put a beeper at the current location                                #
+    #########################################################################################################
+    pass
