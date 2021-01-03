08d4a3822009185bddcbb93b6bd18e7b981f870f
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-01-03 11:18:38 +0100

    bin/karel.bat: Fix typos

--- bin/karel.bat
+++ bin/karel.bat
@@ -1,9 +1,9 @@
 SET scriptpath=%~dp0
 
 if not exist "%scriptpath%\..\venvs\karel" (
-  python3 -m venv "%scriptpath%\..\venvs\karel"
+  python -m venv "%scriptpath%\..\venvs\karel"
 )
 
-"%scriptpath%\..\venvs\karel\Scripts\pip" install --upgrade -r "%scriptspath%\..\common\karel\requirements.txt"
+"%scriptpath%\..\venvs\karel\Scripts\pip" install --upgrade -r "%scriptpath%\..\common\karel\requirements.txt"
 
 "%scriptpath%\..\venvs\karel\Scripts\python" "%scriptpath%\..\common\karel\run.py"
