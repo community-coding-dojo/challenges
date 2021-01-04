0b28f818e99b33ab878cf535427f592dae21f38e
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-01-04 15:31:24 +0100

    advanced/karel: Add 02_advanced_pathfinding
    - Rename 07_another_labyrinth to 01_another_labyrinth

---
+++ advanced/karel/solutions/02_advanced_pathfinding.py
@@ -0,0 +1,37 @@
+from stanfordkarel import *
+
+
+"""
+Karel is getting tired of being lost all the time. Therefore, it want's to learn how to find and remember the shortest 
+way through a maze.
+
+Help Karel to find the shortest way through the labyrinth and mark it with beepers. For testing purposes, 
+a smaller labyrinth map has been created for this task. However, once you're done, this solution should also work
+on the big labyrinth from task 01_another_labyrinth. 
+
+Hint: Placing multiple beepers and counting them might be necessary...
+
+==Leaderboard==
+
+high score: ? Karel commands
+
+"""
+
+
+def main():
+    #########################################################################################################
+    # This function contains the code which will control the karel robot.                                   #
+    # For this task you might need the following functions:                                                 #
+    #   move()                   Move karel one field in the direction it is facing                         #
+    #   turn_left()              Turn karel left (90 degrees)                                               #
+    #   front_is_clear()         Returns True if Karel could move on the field it is facing                 #
+    #   front_is_blocked()       Returns False if Karel could move on the field it is facing                #
+    #   right_is_clear()         Returns True if Karel could move on the field to its right                 #
+    #   right_is_blocked()       Returns False if Karel could move on the field to its right                #
+    #   left_is_clear()          Returns True if Karel could move on the field to its left                  #
+    #   left_is_blocked()        Returns False if Karel could move on the field to its left                 #
+    #   corner_color_is('Green') Returns True if the color of the field which Karel is standing on is GREEN #
+    #   beepers_present()   Returns True if Karel is standing on a beeper                                   #
+    #   put_beeper()        Makes Karel put a beeper at the current location                                #
+    #########################################################################################################
+    pass
